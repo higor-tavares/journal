58debe257c788f35efbee7392ad330c52fee162c
paulo.tavares <paulo.tavares@olxbr.com>
2023-10-31 10:03:39 -0300

    products controller

--- controllers/products.py
+++ controllers/products.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_restful import Resource, Api
+import sys
+
+sys.path.insert(0,'./')
+from models.product import Product
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class Products(Resource):
+    def get(self):
+        return {
+            'products': [
+                Product('xiaomi',1000).get_product()
+        ]}
+
+api.add_resource(Products, '/products')
+
+if __name__ == '__main__':
+    app.run(debug=True)
