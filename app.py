7735f9da2d90b052a20d2f1de803b58760a31567
paulo.tavares <paulo.tavares@olxbr.com>
2024-02-06 07:05:12 -0300

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask_restful import Api
 from controllers.journal_controller import Journal
+from flask_cors import CORS
 
 app = Flask(__name__)
+# Add CORS to the app
+CORS(app)
+cors = CORS(app, resource={
+    r"/*":{
+        "origins":"*"
+    }
+})
+
 api = Api(app)
 
 api.add_resource(Journal, '/')
